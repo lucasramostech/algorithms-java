b25688929f00bb1e4d26b70892a67c970d846c39
lucasramostech_github <lucasramosstar66@gmail.com>
2026-08-09 00:16:14 -0300

    Adding SelectionSort Algorithm

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros = new ArrayList<>(Arrays.asList(84, 12, 57, 3, 91, 46, 28
+            , 70, 15, 63, 39, 8, 95, 21, 54, 77, 6, 42, 88, 33));
+
+            selectionSort(numeros); 
+    }
+    
+    public static void selectionSort(ArrayList<Integer> numeros) {
+        ArrayList<Integer> numerosOrdenados = new ArrayList<>();
+
+        for (int numeroAtual : new ArrayList<>(numeros)) {
+
+            double inverso = 0;
+            double maiorInverso = 0;
+            int menorNumero = 0;
+
+            for (int candidato : numeros) {
+                inverso = (double) 1 / candidato;
+
+                if (inverso > maiorInverso) {
+                    maiorInverso = inverso;
+                    menorNumero = candidato;
+                }
+            }
+
+            numerosOrdenados.add(menorNumero);
+            numeros.remove(Integer.valueOf(menorNumero));
+        }
+
+        System.out.println("Array ordenado: " + numerosOrdenados);
+    }
+}
